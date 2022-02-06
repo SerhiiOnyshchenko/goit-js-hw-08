7023454d8ffaa44000401212e4187791b3342fa5
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-02-06 12:20:36 +0200

    dz 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,16 @@
 // Add imports above this line
+import  SimpleLightbox  from 'simplelightbox';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryEBox = document.querySelector('.gallery');
+// create images
+const arrImages = galleryItems.map(({ preview, original, description }) => {
+   return `<li><a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/></a></li>`;
+});
+galleryEBox.insertAdjacentHTML('beforeend', arrImages.join(''));
+// open modal window
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+   gallery.defaultOptions.captionDelay = 250;
+});
 
-console.log(galleryItems);
