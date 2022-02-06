ac956a631889ed80b1ea701f3c23ff4fa38800c4
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-02-06 13:12:36 +0200

    dz 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+let videoplayerCurrentTime =0
+player.on('timeupdate', function (data) {
+   videoplayerCurrentTime = data.seconds;
+   console.log('seconds:',videoplayerCurrentTime);
+});
+player
+   .setCurrentTime(videoplayerCurrentTime)
+	.then(function (seconds) {
+      // seconds = videoplayerCurrentTime
+   })
+   .catch(function (error) {
+      switch (error.name) {
+         case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+         default:
+            // some other error occurred
+            break;
+      }
+   });
